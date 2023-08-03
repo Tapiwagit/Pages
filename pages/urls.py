d6d4072b2b0a853070815851484990c35e15d994
Tapiwa <91063666+Tapiwagit@users.noreply.github.com>
2023-08-03 20:05:11 +0200

    add contact form

---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import homePageView, aboutPageView, contactPageView
+
+
+urlpatterns = [
+    path("", homePageView, name="home"),
+    path("about/", aboutPageView, name="about"),
+    path("contact/", contactPageView, name="contact"),
+]
---
+++ pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def homePageView(request):
+    return render(request, "home.html")
+
+
+def aboutPageView(request):
+    return render(request, "about.html")
+
+
+def contactPageView(request):
+    return render(request, "contact.html")
